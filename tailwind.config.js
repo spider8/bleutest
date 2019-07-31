8c131aba1ac0e80d51d004386117d2609f0d8392
Anderson Araújo <anderson.araujo.08@gmail.com>
2019-07-31 10:12:52 -0300

    Initial project.

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        bleu: {
+          default: "#0080DD",
+          dark: "#003358"
+        },
+        brow: {
+          lighter: "#DEDEDE",
+          default: "#EFEFEF",
+          dark: "#585858"
+        }
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+}
